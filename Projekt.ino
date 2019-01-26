70ea27803751212da4764752cf6369a546434482
AdrianCygan123 <46976512+AdrianCygan123@users.noreply.github.com>
2019-01-26 18:43:33 +0100

    Add files via upload

---
+++ Projekt.ino
@@ -0,0 +1,191 @@
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h> // biblioteki, ktore pomagają obslużyć I2C
+
+// zadefiniowane wszystkie dźwięki
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+hd44780_I2Cexp lcd;
+// ustawienie wszystkich przycisków
+void setup() {
+  pinMode(13, INPUT);
+  pinMode(12, INPUT);
+  pinMode(11, INPUT);
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+  pinMode(7, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(1, 0);
+  lcd.print("Uniwersalny");
+  lcd.setCursor(3, 1);
+  lcd.print("Stroik");
+}
+
+void loop() {
+// kod główny
+  
+  if (digitalRead(12) == HIGH) //tryb dla przyciska na 12 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna E2");
+    tone(7, NOTE_E2);
+  } 
+        if (digitalRead(11) == HIGH) // tryb dla przyciska na 11 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna A2");
+    tone(7, NOTE_A2);
+  } 
+        if (digitalRead(10) == HIGH) // tryb dla przyciska na 10 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna D3");
+    tone(7, NOTE_D3);
+  } 
+          if (digitalRead(9) == HIGH) // tryb dla przyciska na 9 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna B3");
+    tone(7, NOTE_B3);
+  } 
+          if (digitalRead(8) == HIGH) // tryb dla przyciska na 8 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna E4");
+    tone(7, NOTE_E4);
+  } 
+          if (digitalRead(6) == HIGH) // tryb dla przyciska na 6 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+  lcd.setCursor(1, 0);
+  lcd.print("Struna G3");
+    tone(7, NOTE_G3);
+  } 
+  if (digitalRead(13) == HIGH) // tryb dla przyciska na 6 input
+  {
+     lcd.setCursor(1, 0);
+  lcd.print("            ");
+  lcd.setCursor(3, 1);
+  lcd.print("      ");
+ lcd.setCursor(1, 0);
+  lcd.print("Uniwersalny");
+  lcd.setCursor(3, 1);
+  lcd.print("Stroik");
+    noTone(7);
+  }
+  
+    
+  
+}
